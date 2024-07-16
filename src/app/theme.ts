bce13ccbad8c9adee928c4790e13e6ffcc967464
Thien Nguyen <tatthien.contact@gmail.com>
2024-07-16 22:46:01 +0700

    chore: adjust container size

--- src/app/theme.ts
+++ src/app/theme.ts
@@ -16,16 +16,6 @@ import tabClasses from "./styles/tab.module.css";
 
 const inter = Inter({ subsets: ["latin"] });
 
-const CONTAINER_SIZES: Record<string, string> = {
-  xxs: rem(300),
-  xs: rem(400),
-  sm: rem(500),
-  md: rem(600),
-  lg: rem(700),
-  xl: rem(800),
-  xxl: rem(900),
-};
-
 export const theme = createTheme({
   primaryColor: "dark",
   fontFamily: inter.style.fontFamily,
@@ -47,17 +37,8 @@ export const theme = createTheme({
   components: {
     Container: Container.extend({
       defaultProps: {
-        size: "sm",
+        size: 450,
       },
-      vars: (_, { size, fluid }) => ({
-        root: {
-          "--container-size": fluid
-            ? "100%"
-            : size !== undefined && size in CONTAINER_SIZES
-              ? CONTAINER_SIZES[size]
-              : rem(size),
-        },
-      }),
     }),
     Menu: Menu.extend({
       defaultProps: {
