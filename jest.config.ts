f60b4bcd2323740c4d56e8fe57ca4ebd1f887a33
Thien Nguyen <tatthien.contact@gmail.com>
2024-07-25 18:44:57 +0700

    test: init

---
+++ jest.config.ts
@@ -0,0 +1,12 @@
+/**
+ * For a detailed explanation regarding each configuration property, visit:
+ * https://jestjs.io/docs/configuration
+ */
+
+import type { Config } from 'jest';
+
+const config: Config = {
+  clearMocks: true,
+};
+
+export default config;
